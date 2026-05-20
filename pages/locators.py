e9db58237f492cb7cd146e4cf2a7f68c684f2fca
Andrey Dolgachev <andrei.dolga4ev@yandex.ru>
2026-05-20 17:42:31 +0300

    Adding login page class, methods and locators

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,13 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.ID, "login_form")
+    LOGIN_INPUT_EMAIL = (By.ID, "id_login-username")
+    LOGIN_INPUT_PASSWORD = (By.ID, "id_login-password")
+    REGISTRATION_FORM = (By.ID, "register_form")
+    REGISTRATION_INPUT_EMAIL = (By.ID, "id_registration-email")
+    REGISTRATION_INPUT_PASSWORD1 = (By.ID, "id_registration-password1")
+    REGISTRATION_INPUT_PASSWORD2 = (By.ID, "id_registration-password2")
