1165d5971aaaf707ff76401089d86aa2572aec5f
Andrey Dolgachev <andrei.dolga4ev@yandex.ru>
2026-05-26 10:39:25 +0300

    Refactor product page test

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,14 +3,17 @@ from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
+    def get_product_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        return product_name
+
     def click_add_to_basket_btn(self):
         btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
         btn.click()
 
-    def should_be_product_name_in_message(self):
-        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+    def should_be_product_name_in_message(self, product_name):
         successful_adding_message = self.browser.find_element(*ProductPageLocators.MESSAGE_SUCCESSFUL_ADDING).text
-        assert product_name in successful_adding_message, "Название товара не соответсвует названию в уведомлении"
+        assert product_name == successful_adding_message, "Название товара не соответсвует названию в уведомлении"
 
     def should_be_product_price_in_message(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
